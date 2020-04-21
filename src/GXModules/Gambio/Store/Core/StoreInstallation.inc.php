ed231dbbd4824158bc0090ca2d2de4f0ebebcac3
Alex Tselegidis <alextselegidis@gmail.com>
2020-04-21 18:51:46 +0200

    created the StoreInstallation.inc.php and StoreRemoval.inc.php

---
+++ src/GXModules/Gambio/Store/Core/StoreInstallation.inc.php
@@ -0,0 +1,25 @@
+<?php
+/* --------------------------------------------------------------
+   StoreInstallation.inc.php 2020-04-21
+   Gambio GmbH
+   http://www.gambio.de
+   Copyright (c) 2020 Gambio GmbH
+   Released under the GNU General Public License (Version 2)
+   [http://www.gnu.org/licenses/gpl-2.0.html]
+   --------------------------------------------------------------
+*/
+
+/**
+ * Class StoreInstallation
+ *
+ * Performs a Store package installation and take care of all the required actions.
+ *
+ * Execute the upgrade script if needed.
+ */
+class StoreInstallation
+{
+    public function perform()
+    {
+        
+    }
+}
---
+++ src/GXModules/Gambio/Store/Core/StoreRemoval.inc.php
@@ -0,0 +1,25 @@
+<?php
+/* --------------------------------------------------------------
+   StoreRemoval.inc.php 2020-04-21
+   Gambio GmbH
+   http://www.gambio.de
+   Copyright (c) 2020 Gambio GmbH
+   Released under the GNU General Public License (Version 2)
+   [http://www.gnu.org/licenses/gpl-2.0.html]
+   --------------------------------------------------------------
+*/
+
+/**
+ * Class StoreRemoval
+ *
+ * Performs a Store package removal and take care of all the required actions.
+ *
+ * Execute the downgrade script if needed.
+ */
+class StoreRemoval
+{
+    public function perform()
+    {
+        
+    }
+}
