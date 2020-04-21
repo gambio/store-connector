53886c51430a606acaf616637685fd82fb491055
Alex Tselegidis <alextselegidis@gmail.com>
2020-04-21 18:43:54 +0200

    created the StoreUpgrade.inc.php file

---
+++ src/GXModules/Gambio/Store/Core/StoreUpgrade.inc.php
@@ -0,0 +1,27 @@
+<?php
+/* --------------------------------------------------------------
+   StoreUpgrade.inc.php 2020-04-21
+   Gambio GmbH
+   http://www.gambio.de
+   Copyright (c) 2020 Gambio GmbH
+   Released under the GNU General Public License (Version 2)
+   [http://www.gnu.org/licenses/gpl-2.0.html]
+   --------------------------------------------------------------
+*/
+
+/**
+ * Class StoreUpgrade
+ *
+ * This file holds all the upgrade operations required after a Store Connector installation.
+ *
+ * Set this file as the "upgrade" script of the "store.json".
+ */
+class StoreUpgrade
+{
+    public static function run()
+    {
+        
+    }
+}
+
+StoreUpgrade::run();
