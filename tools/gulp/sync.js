635ba85466ad17bb413dc4d83ef29f28bcbd4eaa
c.schmidt <c.schmidt@gambio.de>
2020-04-30 10:59:00 +0200

    Task #5487 sync.js  - Gulp task update

--- tools/gulp/sync.js
+++ tools/gulp/sync.js
@@ -2,7 +2,7 @@
  doc.js 2020-04-30
  Gambio GmbH
  http://www.gambio.de
- Copyright (c) 2018 Gambio GmbH
+ Copyright (c) 2020 Gambio GmbH
  Released under the GNU General Public License (Version 2)
  [http://www.gnu.org/licenses/gpl-2.0.html]
  --------------------------------------------------------------
