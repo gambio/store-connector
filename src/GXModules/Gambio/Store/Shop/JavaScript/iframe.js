43053a36b8c8092a8efde08e53ba9abef2deb9e9
c.schmidt <c.schmidt@gambio.de>
2020-04-30 12:55:54 +0200

    Task #5511 Create build function in to the iframe.js

--- src/GXModules/Gambio/Store/Shop/JavaScript/iframe.js
+++ src/GXModules/Gambio/Store/Shop/JavaScript/iframe.js
@@ -1,5 +1,28 @@
-const iframe = {
+import translation from './translation'
 
+/**
+ * Builds the Iframe into the html document with store url and language code.
+ *
+ * @return {Promise<void>}
+ */
+const build = async () => {
+	const storeUrl = shop.getStoreUrl();
+	const iframe = document.createElement('iframe');
+	const languageCode = translation.getLanguageCode();
+	
+	if (storeUrl.includes('?')) {
+		iframe.src = `&language=${languageCode}`;
+	} else {
+		iframe.src = `?language=${languageCode}`;
+	}
+	
+	iframe.id = 'storeIframe';
+	iframe.style.width = '100%';
+	iframe.style.height = '100%';
+	iframe.style.border = 'none';
+	iframe.style.overflow = 'hidden';
+	
+	iframe.addEventListener('load', resolve);
+	
+	parent.appendChild(iframe);
 }
-
-export default iframe;
