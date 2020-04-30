9eb98af57164e1204d1a34b46b2eca96922d4967
Andrey Filippov <a.filippov@gambio.de>
2020-04-30 11:04:04 +0200

    Task #5485: Updated styles.js

--- tools/gulp/styles.js
+++ tools/gulp/styles.js
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------
- gulp_styles.js 2018-11-02
+ styles.js 2020-04-30
  Gambio GmbH
  http://www.gambio.de
- Copyright (c) 2018 Gambio GmbH
+ Copyright (c) 2020 Gambio GmbH
  Released under the GNU General Public License (Version 2)
  [http://www.gnu.org/licenses/gpl-2.0.html]
  --------------------------------------------------------------
@@ -23,7 +23,6 @@
 module.exports = function(gulp, $) {
 	const fs = require('fs');
 	const path = require('path');
-	const environment = require('./lib/environment');
 	
 	const isDir = (file) => {
 		return fs.lstatSync(file.path).isDirectory();
@@ -33,44 +32,28 @@ module.exports = function(gulp, $) {
 		return (path.extname(file.path) === '.html');
 	};
     
-    const compile = (variant, vendorName, moduleName) => {
+    const compile = () => {
         return new Promise((resolve, reject) => {
             gulp.src([
-                `${variant}/GXModules/${vendorName}/${moduleName}/**/*.css`,
-                `!${variant}/GXModules/${vendorName}/${moduleName}/**/*.min.css`,
-                `!${variant}/GXModules/${vendorName}/${moduleName}/Build/**`,
-                `!${variant}/GXModules/${vendorName}/${moduleName}/**/Templates/**`,
+                `src/GXModules/Gambio/Store/**/*.css`,
+                `!src/GXModules/Gambio/Store/**/*.min.css`,
+                `!src/GXModules/Gambio/Store/Build/**`,
+                `!src/GXModules/Gambio/Store/**/Templates/**`,
             ])
                 .on('finish', resolve)
                 .on('error', reject)
-                .pipe($.changed(`${variant}/GXModules/${vendorName}/${moduleName}/Build`))
-                .pipe(gulp.dest(`${variant}/GXModules/${vendorName}/${moduleName}/Build`))
+                .pipe($.changed(`src/GXModules/Gambio/Store/Build`))
+                .pipe(gulp.dest(`src/GXModules/Gambio/Store/Build`))
                 .pipe($.ignore.exclude(isDir))
                 .pipe($.ignore.exclude(isHtml))
                 .pipe($.cleanCss())
                 .pipe($.rename({suffix: '.min'}))
-                .pipe(gulp.dest(`${variant}/GXModules/${vendorName}/${moduleName}/Build`));
+                .pipe(gulp.dest(`src/GXModules/Gambio/Store/Build`));
         });
     };
 	
 	return (async) => {
-		const variant = environment.getArgument('variant') || 'src';
-		
-		const vendorNames = fs.readdirSync(variant + '/GXModules')
-			.filter(file => fs.statSync(path.join(variant + '/GXModules', file)).isDirectory());
-		
-		const compilations = [];
-		
-		for (let vendorName of vendorNames) {
-			const moduleNames = fs.readdirSync(variant + '/GXModules/' + vendorName)
-				.filter(file => fs.statSync(path.join(variant + '/GXModules', vendorName, file)).isDirectory());
-			
-			for (let moduleName of moduleNames) {
-			    compilations.push(compile(variant, vendorName, moduleName));
-			}
-		}
-		
-		Promise.all(compilations)
+        compile()
             .then(() => async())
             .catch((error) => {
                 $.util.log($.util.colors.red(`Unexpected styles compilation error: ${error}`));
