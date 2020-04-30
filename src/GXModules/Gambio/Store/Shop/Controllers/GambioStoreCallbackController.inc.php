97f4f97df2ad86b257928391fd4cb035a0e909cf
k.sekin <k.sekin@gambio.de>
2020-04-30 12:33:21 +0200

    Task #5507: GambioStoreCallbackController.inc.php - actionDefault Task #5508: GambioStoreCallbackController.inc.php - actionVerify Task #5547: Create the verifyStoreToken method.

---
+++ src/GXModules/Gambio/Store/Shop/Controllers/GambioStoreCallbackController.inc.php
@@ -0,0 +1,69 @@
+<?php
+/* --------------------------------------------------------------
+   GambioStoreCallbackController.inc.php 2019-04-02
+   Gambio GmbH
+   http://www.gambio.de
+   Copyright (c) 2019 Gambio GmbH
+   Released under the GNU General Public License (Version 2)
+   [http://www.gnu.org/licenses/gpl-2.0.html]
+   --------------------------------------------------------------
+*/
+
+/**
+ * Class GambioStoreCallbackController
+ *
+ * Allows for Callbacks to the Shop from the Gambio Store API
+ *
+ * @category System
+ * @package  AdminHttpViewControllers
+ */
+class GambioStoreCallbackController extends HttpViewController
+{
+    /**
+     * @var \GambioStoreConnector
+     */
+    private $connector;
+    
+    
+    /**
+     * Setup of our Connector classes
+     */
+    private function setup()
+    {
+        $this->connector = GambioStoreConnector::getInstance();
+    }
+    
+    
+    /**
+     * Currently not implemented
+     *
+     * @return \JsonHttpControllerResponse
+     */
+    public function actionDefault()
+    {
+        
+        $response = [
+            'success' => false,
+            'notice'  => 'Method not implemented'
+        ];
+        
+        return MainFactory::create("JsonHttpControllerResponse", $response);
+    }
+    
+    
+    /**
+     * Verifies whether the Token that the Gambio Store received is the same token that is stored in this Shop
+     *
+     * @return \JsonHttpControllerResponse
+     */
+    public function actionVerify()
+    {
+        $storeToken = $this->_getPostData('storeToken');
+        
+        $result = $this->connector->verifyStoreToken($storeToken);
+    
+        return MainFactory::create('JsonHttpControllerResponse', [
+            'success' => $result
+        ]);
+    }
+}
