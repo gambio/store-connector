75dcb70cbf3caef3b0429718567f5fd5b4598477
a.winter <a.winter@gambio.de>
2020-04-30 13:34:20 +0200

    Task #5539: shop.js now initializes listeners.

--- src/GXModules/Gambio/Store/Shop/JavaScript/shop.js
+++ src/GXModules/Gambio/Store/Shop/JavaScript/shop.js
@@ -13,61 +13,60 @@ import messenger from './messenger';
 /**
  * ## Shop
  *
- * This controller will handles the frontend logic of the Gambio Store page.
+ * This controller controls the shop page.
  *
  * @module Controllers/gambio_store
  */
-export default function({storeToken}) {
-    return {
-        /**
-         * Return shop information
-         *
-         * @returns {Promise<Object>}
-         */
-        fetchShopInfo: function() {
-            return fetch('admin.php?do=GambioStoreAjax/collectShopInformation');
-        },
-        
-        /**
-         * Return whether the data processing has been accepted.
-         *
-         * @returns {Promise<Object>}
-         */
-        isDataProcessingAccepted: function() {
-            return fetch('admin.php?do=GambioStoreAjax/isDataProcessingAccepted');
-        },
-        
-        /**
-         * Attach message event listener
-         */
-        listenToMessages: function() {            
-            messenger.listenToMessage('update_shop_information', function() {
-                fetchShopInfo().then(sendShopInfo);
-            });
-            messenger.listenToMessage('request_shop_information', function() {
-                fetchShopInfo().then(sendShopInfo);
-            });
-            
-            messenger.listenToMessage('request_store_token', function() {
-                messenger.sendMessage('send_store_token', {'storeToken': storeToken});
-            });
-            
-            messenger.listenToMessage('send_data_processing_accepted', function() {
-                window.location.href = 'admin.php?do=GambioStore/AcceptDataProcessing';
-            });
-            
-            messenger.listenToMessage('reload_page', function() {
-                window.location.reload(true);
-            });
-        },
-        
-        /**
-         * Send shop information data to Iframe
-         *
-         * @param {Object} shopInfo Shop information data
-         */
-        sendShopInfo: function(shopInfo) {
-            messenger.sendMessage('send_shop_information', {shopInfo})
-        }
+const shop = {
+    /**
+     * Return shop information
+     *
+     * @returns {Promise<Object>}
+     */
+    fetchShopInfo: function() {
+        return fetch('admin.php?do=GambioStoreAjax/collectShopInformation');
+    },
+    
+    /**
+     * Return whether the data processing has been accepted.
+     *
+     * @returns {Promise<Object>}
+     */
+    isDataProcessingAccepted: function() {
+        return fetch('admin.php?do=GambioStoreAjax/isDataProcessingAccepted');
+    },
+    
+    /**
+     * Send shop information data to Iframe
+     *
+     * @param {Object} shopInfo Shop information data
+     */
+    sendShopInfo: function(shopInfo) {
+        messenger.sendMessage('send_shop_information', {shopInfo})
     }
-}
+};
+export default shop;
+
+/**
+ * Initiate messenger listeners upon a built document.
+ */
+window.addEventListener('DOMContentLoaded', () => {
+    messenger.listenToMessage('update_shop_information', function() {
+        shop.fetchShopInfo().then(sendShopInfo);
+    });
+    messenger.listenToMessage('request_shop_information', function() {
+        shop.fetchShopInfo().then(sendShopInfo);
+    });
+    
+    messenger.listenToMessage('request_store_token', function() {
+        messenger.sendMessage('send_store_token', {'storeToken': storeToken});
+    });
+    
+    messenger.listenToMessage('send_data_processing_accepted', function() {
+        window.location.href = 'admin.php?do=GambioStore/AcceptDataProcessing';
+    });
+    
+    messenger.listenToMessage('reload_page', function() {
+        window.location.reload(true);
+    });
+});
