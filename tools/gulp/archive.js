3b1d58fceb9eb2f52e47092ddf504014cddf3fca
c.schmidt <c.schmidt@gambio.de>
2020-04-30 11:07:51 +0200

    Updated the licences of the other gulp files

--- tools/gulp/archive.js
+++ tools/gulp/archive.js
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------
- archive.js 2018-11-29
+ archive.js 2020-04-30
  Gambio GmbH
  http://www.gambio.de
- Copyright (c) 2018 Gambio GmbH
+ Copyright (c) 2020 Gambio GmbH
  Released under the GNU General Public License (Version 2)
  [http://www.gnu.org/licenses/gpl-2.0.html]
  --------------------------------------------------------------
--- tools/gulp/lib/environment.js
+++ tools/gulp/lib/environment.js
@@ -1,8 +1,8 @@
 /* --------------------------------------------------------------
- environment.js 2018-12-03
+ environment.js 2020-04-30
  Gambio GmbH
  http://www.gambio.de
- Copyright (c) 2018 Gambio GmbH
+ Copyright (c) 2020 Gambio GmbH
  Released under the GNU General Public License (Version 2)
  [http://www.gnu.org/licenses/gpl-2.0.html]
  --------------------------------------------------------------
