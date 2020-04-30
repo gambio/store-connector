f47e9c555724e09048602e66ab5741a8e56c57b4
Andrey Filippov <a.filippov@gambio.de>
2020-04-30 18:06:19 +0200

    Task #5553: GambioStoreCompatibility class

--- src/GXModules/Gambio/Store/Core/GambioStoreCompatibility.inc.php
+++ src/GXModules/Gambio/Store/Core/GambioStoreCompatibility.inc.php
@@ -22,7 +22,20 @@ require __DIR__ . '/GambioStoreDatabase.inc.php';
  */
 class GambioStoreCompatibility
 {
+    /**
+     *
+     */
     const RESOURCE_GM_CONFIGURATION_TABLE = 'gm_configuration';
+    
+    /**
+     *
+     */
+    const FEATURE_THEME_CONTROL = 'themeControl';
+    
+    /**
+     *
+     */
+    const FEATURE_THEME_SERVICE = 'themeService';
     
     
     /**
@@ -45,7 +58,37 @@ class GambioStoreCompatibility
      */
     public function has($resource)
     {
+        switch($resource) {
+            case self::RESOURCE_GM_CONFIGURATION_TABLE:
+                return $this->doesGxConfigurationTableExists();
     
+            case self::FEATURE_THEME_CONTROL:
+                return $this->doesFeatureThemeControlExists();
+    
+            case self::FEATURE_THEME_SERVICE:
+                return $this->doesFeatureThemeServiceExists();
+                
+            default: return false;
+        }
+    }
+    
+    private function doesGxConfigurationTableExists()
+    {
+        $sql = 'SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = :database AND table_name = :table_name;';
+
+        $query = $this->database->query($sql, [':database' => DB_DATABASE, ':table_name' => self::RESOURCE_GM_CONFIGURATION_TABLE]);
+        
+        return (bool) $query->rowCount();
+    }
+    
+    private function doesFeatureThemeControlExists()
+    {
+        return method_exists('StaticGXCoreLoader','getThemeControl');
+    }
+    
+    private function doesFeatureThemeServiceExists()
+    {
+        return class_exists('ThemeService');
     }
 }
 
