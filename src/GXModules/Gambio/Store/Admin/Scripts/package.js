f23b3cf79a45c2da844d257128b501998cfc90a6
c.schmidt <c.schmidt@gambio.de>
2020-05-04 19:22:20 +0200

    WIP: Correcting package.js

--- src/GXModules/Gambio/Store/Admin/Scripts/package.js
+++ src/GXModules/Gambio/Store/Admin/Scripts/package.js
@@ -1,5 +1,5 @@
 /* --------------------------------------------------------------
-   package.js 2020-04-30
+   package.js 2020-05-04
    Gambio GmbH
    http://www.gambio.de
    Copyright (c) 2020 Gambio GmbH
@@ -9,19 +9,20 @@
  */
 import messenger from './messenger'
 import callShop from './callShop'
+import translation from './translation'
 
 /**
  * @param folderNameInsideShop
  */
 const activateTheme = (folderNameInsideShop) => {
-    const formData = new FormData();
-    formData.append('folderNameInsideShop', folderNameInsideShop);
-    
-    callShop('./admin.php?do=GambioStoreAjax/ActivateTheme', {
-        method: 'POST',
-        body: formData
-    }).then(() => messenger.sendMessage('activation_succeeded'))
-        .catch(() => messenger.sendMessage('activation_failed'));
+	const formData = new FormData();
+	formData.append('folderNameInsideShop', folderNameInsideShop);
+	
+	callShop('./admin.php?do=GambioStoreAjax/ActivateTheme', {
+		method: 'POST',
+		body: formData
+	}).then(() => messenger.sendMessage('activation_succeeded'))
+		.catch(() => messenger.sendMessage('activation_failed'));
 }
 
 /**
@@ -29,7 +30,7 @@ const activateTheme = (folderNameInsideShop) => {
  * @returns {Promise<Response>}
  */
 const isThemeActive = (themeName) => {
-    return callShop('admin.php?do=GambioStoreAjax/IsThemeActive&themeName=' + themeName);
+	return callShop('admin.php?do=GambioStoreAjax/IsThemeActive&themeName=' + themeName);
 };
 
 /**
@@ -41,42 +42,39 @@ const isThemeActive = (themeName) => {
  * @param progressCallback {function} invoked between each installation request. Progress-Bars may hook into this.
  * @returns {Promise<unknown>} Resolves when installed. Rejects upon error.
  */
-const installPackage = (data, progressCallback = () => null) => {
-    const formData = new FormData();
-    formData.append('gambioStoreData', JSON.stringify(data));
-    
-    const doPackageInstallation = new Promise((resolve, reject) => {
-        callShop('admin.php?do=GambioStoreAjax/InstallPackage', {
-            method: 'post',
-            body: formData
-        }).then(response => {
-            progressCallback(response);
-            if (response.done === true) {
-                resolve();
-            } else {
-                doPackageInstallation.then(resolve).catch(reject);
-            }
-        }).catch(reject);
-    })
-    
-    return doPackageInstallation;
+const installPackage = async (data, progressCallback = () => null) => {
+	const formData = new FormData();
+	formData.append('gambioStoreData', JSON.stringify(data));
+	
+	const doPackageInstallation = async () => {
+		const response = await callShop('admin.php?do=GambioStoreAjax/InstallPackage', {
+			method: 'post',
+			body: formData
+		});
+		progressCallback(response);
+		
+		if (response.done !== true) {
+			await doPackageInstallation;
+		}
+	}
 }
 
 /**
  * Ensure that file permissions for a theme installation are valid.
  *
  * @param data
- * @returns {Promise<unknown>}
+ * @returns {Promise<boolean>}
  */
-const checkFilePermissions = (data) => {
-    return new Promise(((resolve, reject) => {
-        const formData = new FormData();
-        formData.append('gambioStoreData', JSON.stringify(data));
-        
-        callShop('admin.php?do=GambioStoreAjax/checkPermission', {method: 'post', body: formData})
-            .then(resolve)
-            .catch(reject);
-    }))
+const isFilePermissionCorrect = async (data) => {
+	const formData = new FormData();
+	
+	formData.append('gambioStoreData', JSON.stringify(data));
+	try {
+		await callShop('admin.php?do=GambioStoreAjax/checkPermission', {method: 'post', body: formData})
+		return true;
+	} catch {
+		return false;
+	}
 }
 
 /**
@@ -85,75 +83,92 @@ const checkFilePermissions = (data) => {
  * @param {String} folderNameInsideShop Theme data
  */
 const uninstallPackage = (folderNameInsideShop) => {
-    const formData = new FormData();
-    formData.append('folderNameInsideShop', folderNameInsideShop)
-    
-    callShop('admin.php?do=GambioStoreAjax/uninstallPackage', {method: 'post', body: formData})
-        .then(() => messenger.sendMessage('uninstall_succeeded'))
-        .catch(() => messenger.sendMessage('uninstall_failed', data))
+	const formData = new FormData();
+	formData.append('folderNameInsideShop', folderNameInsideShop)
+	
+	callShop('admin.php?do=GambioStoreAjax/uninstallPackage', {method: 'post', body: formData})
+		.then(() => messenger.sendMessage('uninstall_succeeded'))
+		.catch(() => messenger.sendMessage('uninstall_failed', data))
 }
 
-const install = (data) => {
-    const $installingPackageModal = $('.installing-package.modal');
-    const $progressDescription = $installingPackageModal.find('.progress-description');
-    const $progressBar = $installingPackageModal.find('.progress .progress-bar');
-    
-    $progressDescription.text(jse.core.lang.translate('PREPARING_PACKAGE', 'gambio_store'));
-    
-    const updateProgressCallback = ({progress}) => {
-        let progressPercentage = Math.ceil(progress * 100);
-        
-        if (progressPercentage < 0) {
-            progressPercentage = 0;
-        } else if (progressPercentage > 100) {
-            progressPercentage = 100;
-        }
-        
-        $progressBar.prop('aria-valuenow', progressPercentage);
-        $progressBar.css('width', progressPercentage + '%');
-        $progressBar.text(progressPercentage + '%');
-    };
-    
-    updateProgressCallback({progress: 0}); // always set to 0 initially
-    
-    $installingPackageModal.modal('show');
-    
-    checkFilePermissions(data).then(() => {
-            $progressDescription.text(jse.core.lang.translate('INSTALLING_PACKAGE', 'gambio_store'));
-            installPackage(data, updateProgressCallback)
-                .then(() => {
-                    // By checking whether a gallery object is present,
-                    // we can determine if this is a theme or not.
-                    if (data.details.gallery) {
-                        // find out if the installed theme was an update of the currently active theme
-                        isThemeActive(data.details.folderNameInsideShop)
-                            .then(response => {
-                                if (response.isActive === true) {
-                                    // and if so, activate it again for the content manager entries
-                                    activateTheme(data.details.folderNameInsideShop)
-                                        .then(() => messenger.endMessage('installation_succeeded'))
-                                        .catch(() => messenger.sendMessage('installation_failed'))
-                                } else {
-                                    messenger.sendMessage('installation_succeeded')
-                                }
-                            }).catch(() => messenger.sendMessage('installation_failed'));
-                    } else {
-                        messenger.sendMessage('installation_succeeded')
-                    }
-                })
-                .catch(() => messenger.sendMessage('installation_failed'))
-                .finally(() => {
-                    updateProgressCallback({progress: 1});
-                    setTimeout(() => {
-                        $installingPackageModal.modal('hide');
-                    }, 2000);
-                });
-        }
-    ).catch(() => messenger.sendMessage('ftp_data_requested'));
+/**
+ * Starting the installation process that needs to be done for a package installation to the shop.
+ * @param data
+ * @return {Promise<void>}
+ */
+const startPackageInstallation = async (data) => {
+	// By checking whether a gallery object is present,
+	// we can determine if this is a theme or not.
+	try {
+		$progressDescription.text(translation.translate('INSTALLING_PACKAGE'));
+		await installPackage(data, updateProgressCallback);
+		
+		if (data.details.gallery) {
+			const response = isThemeActive(data.details.folderNameInsideShop);
+			if (response.isActive === true) {
+				await activateTheme(data.details.folderNameInsideShop);
+			}
+		}
+		
+		messenger.sendMessage('installation_succeeded')
+	} catch {
+		messenger.sendMessage('installation_failed');
+	} finally {
+		updateProgressCallback({progress: 1});
+		setTimeout(() => {
+			$installingPackageModal.modal('hide');
+		}, 2000);
+	}
+}
+
+/**
+ * Callback function to update the progressbar in the gui.
+ * @param progress
+ */
+const updateProgressCallback = ({progress}) => {
+	const $installingPackageModal = $('.installing-package.modal');
+	const $progressBar = $installingPackageModal.find('.progress .progress-bar');
+	
+	let progressPercentage = Math.ceil(progress * 100);
+	
+	if (progressPercentage < 0) {
+		progressPercentage = 0;
+	} else if (progressPercentage > 100) {
+		progressPercentage = 100;
+	}
+	
+	$progressBar.prop('aria-valuenow', progressPercentage);
+	$progressBar.css('width', progressPercentage + '%');
+	$progressBar.text(progressPercentage + '%');
+};
+
+/**
+ * Install process to install packages by data.
+ *
+ * @param data
+ * @return {Promise<void>}
+ */
+const install = async (data) => {
+	const $installingPackageModal = $('.installing-package.modal');
+	const $progressDescription = $installingPackageModal.find('.progress-description');
+	
+	$progressDescription.text(translation.translate('PREPARING_PACKAGE'));
+	
+	updateProgressCallback({progress: 0}); // always set to 0 initially
+	$installingPackageModal.modal('show');
+	
+	const filePermission = await isFilePermissionCorrect(data);
+	
+	if (filePermission === false) {
+		messenger.sendMessage('ftp_data_requested');
+		return;
+	}
+	
+	await startPackageInstallation(data);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    messenger.listenToMessage('start_installation_process', install);
-    messenger.listenToMessage('uninstall_theme', data => uninstallPackage(data.fileName));
-    messenger.listenToMessage('activate_theme', data => activateTheme(data.fileName));
+	messenger.listenToMessage('start_installation_process', install);
+	messenger.listenToMessage('uninstall_theme', data => uninstallPackage(data.fileName));
+	messenger.listenToMessage('activate_theme', data => activateTheme(data.fileName));
 });
