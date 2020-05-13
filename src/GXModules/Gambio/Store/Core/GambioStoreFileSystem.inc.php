e5ace0994d552973d30d0af9ad1d9169d26b1ef7
Andrey Filippov <a.filippov@gambio.de>
2020-05-13 14:42:35 +0200

    Task #5679: WIP - GambioStoreFileSystem

---
+++ src/GXModules/Gambio/Store/Core/GambioStoreFileSystem.inc.php
@@ -0,0 +1,65 @@
+<?php
+/* --------------------------------------------------------------
+   GambioStoreFileSystem.inc.php 2020-05-13
+   Gambio GmbH
+   http://www.gambio.de
+   Copyright (c) 2020 Gambio GmbH
+   --------------------------------------------------------------
+*/
+
+class GambioStoreFileSystem
+{
+    
+    public function fileCopy($sourcePath, $destinationPath)
+    {
+    
+    }
+
+    public function dirCopy($sourcePath, $destinationPath)
+    {
+    
+    }
+    
+    public function createDirRecursively()
+    {
+    
+    }
+    
+    public function move($sourcePath, $destinationPath)
+    {
+    
+    }
+    
+    
+    /**
+     * @param $path
+     *
+     * @return bool
+     * @throws \CreateDirectoryException
+     */
+    public function createDirectory($path)
+    {
+        if (!mkdir($path, 0777, true) && !is_dir($path)) {
+            
+            if (is_file($path)) {
+                throw new CreateDirectoryException('Could not create a folder ' . $path, 1, [
+                    'info' => 'There is already a file exists for the path: ' . $path
+                ]);
+            }
+            
+            if (is_link($path)) {
+                throw new CreateDirectoryException('Could not create a folder ' . $path, 2, [
+                    'info' => 'There is already a symlink exists for this path! ' . $path
+                ]);
+            }
+    
+            throw new CreateDirectoryException('Could not create a folder ' . $path, 3, [
+                'info' => 'Please contact the server administrator'
+            ]);
+        }
+        
+        return true;
+    }
+    
+}
+
