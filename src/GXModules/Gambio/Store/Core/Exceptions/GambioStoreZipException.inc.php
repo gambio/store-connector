684e6a7e0e90d0b28a6478d8d7c12205fd5efdab
Andrey Filippov <a.filippov@gambio.de>
2020-05-19 14:48:40 +0200

    Throw exception if unzip fails.

---
+++ src/GXModules/Gambio/Store/Core/Exceptions/GambioStoreZipException.inc.php
@@ -0,0 +1,18 @@
+<?php
+/* --------------------------------------------------------------
+GambioStoreZipException.inc.php 2020-05-19
+Gambio GmbH
+http://www.gambio.de
+Copyright (c) 2020 Gambio GmbH
+Released under the GNU General Public License (Version 2)
+[http://www.gnu.org/licenses/gpl-2.0.html]
+--------------------------------------------------------------
+*/
+
+/**
+ * Class GambioStoreZipException
+ */
+class GambioStoreZipException extends Exception
+{
+
+}
