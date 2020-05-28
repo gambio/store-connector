9fb232dd4a027be47b4e515554db9c91a4a7841f
c.schmidt <c.schmidt@gambio.de>
2020-05-28 14:59:38 +0200

    Correct exception extending

--- src/GXModules/Gambio/Store/Core/Exceptions/GambioStoreZipException.inc.php
+++ src/GXModules/Gambio/Store/Core/Exceptions/GambioStoreZipException.inc.php
@@ -1,18 +1,20 @@
 <?php
 /* --------------------------------------------------------------
-GambioStoreZipException.inc.php 2020-05-19
-Gambio GmbH
-http://www.gambio.de
-Copyright (c) 2020 Gambio GmbH
-Released under the GNU General Public License (Version 2)
-[http://www.gnu.org/licenses/gpl-2.0.html]
+  GambioStoreZipException.inc.php 2020-05-28
+  Gambio GmbH
+  http://www.gambio.de
+  Copyright (c) 2020 Gambio GmbH
+  Released under the GNU General Public License (Version 2)
+  [http://www.gnu.org/licenses/gpl-2.0.html]
 --------------------------------------------------------------
 */
+
+require_once 'GambioStoreException.inc.php';
 
 /**
  * Class GambioStoreZipException
  */
-class GambioStoreZipException extends Exception
+class GambioStoreZipException extends GambioStoreException
 {
 
 }
