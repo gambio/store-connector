635cab8e9398c7652770b8f6f5e2a637e2bd4f30
c.schmidt <c.schmidt@gambio.de>
2020-06-15 14:15:39 +0200

    Task #5805 Adjust the swap_hub_urls php script to use the new environment file

--- docker/boilerplate/swap_store_urls.php
+++ docker/boilerplate/swap_store_urls.php
@@ -7,11 +7,11 @@ $pass = 'gxdev';
 
 $pdo = new \PDO($dsn, $user, $pass);
 
-$pdo->query("UPDATE configuration SET configuration_value='http://172.17.0.1:6100/public/api.php/api/v1' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_URL'");
+$pdo->query("UPDATE configuration SET configuration_value='http://" . $_ENV['DOCKER_HOST_IP'] . ":6100/public/api.php/api/v1' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_URL'");
 
-$pdo->query("UPDATE configuration SET configuration_value='http://172.17.0.1:6300/' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_SETTINGS_APP_URL'");
+$pdo->query("UPDATE configuration SET configuration_value='http://" . $_ENV['DOCKER_HOST_IP'] . ":6300/' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_SETTINGS_APP_URL'");
 
-$pdo->query("UPDATE configuration SET configuration_value='http://172.17.0.1:6400/' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_ACCOUNT_APP_URL'");
+$pdo->query("UPDATE configuration SET configuration_value='http://" . $_ENV['DOCKER_HOST_IP'] . ":6400/' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_ACCOUNT_APP_URL'");
 
 $pdo->query("UPDATE configuration SET configuration_value='https://core-api.gambiohub.com/trust/hub_hosts.json_' WHERE configuration_key='MODULE_PAYMENT_GAMBIO_HUB_IP_LIST_URL'");
 
