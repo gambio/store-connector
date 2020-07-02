babe089e086a69ecb4f808b7d7a38265a9c2e3f0
c.schmidt <c.schmidt@gambio.de>
2020-07-02 16:15:01 +0200

    Task #5904 In scripts/start.sh, remove "hide_office_network_from_docker" and "increase_system_resources_for_elasticsearch" as they will not be needed anymore

--- docker/boilerplate/prestart.sh
+++ docker/boilerplate/prestart.sh
@@ -15,18 +15,6 @@ inform_user() {
 
 }
 
-hide_office_network_from_docker() {
-
-	inform_user "Hiding the internal office network from docker..."
-
-	NAMESERVER_LINE="nameserver $(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')"
-	NAMESERVER_FILE=/etc/resolv.conf
-	grep -qF "$NAMESERVER_LINE" "$NAMESERVER_FILE" || echo "$NAMESERVER_LINE" | sudo tee --append "$NAMESERVER_FILE"
-
-}
-
 inform_user "Configuring operating system (root permissions may be necessary) ..."
-
-hide_office_network_from_docker
 
 inform_user "Starting containers..."
--- docker/boilerplate/start.sh
+++ docker/boilerplate/start.sh
@@ -4,4 +4,4 @@
 
 export DOCKER_HOST_IP="$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')"
 
-docker-compose up
+docker-compose up -d
