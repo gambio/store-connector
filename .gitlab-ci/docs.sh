27466e421fa307fb18eda5612fc2114b48656a9d
a.winter <a.winter@gambio.de>
2020-07-22 17:59:27 +0200

    Task #6018: moved docs subdirectory to docs/referencbash docs/generte-docs.sh

---
+++ .gitlab-ci/docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+inform_user() {
+	if [[  -z "$1" ]]; then
+		print_usage "Empty argument provided"
+		exit 1
+	fi
+
+	local GREEN_COLOR="\033[0;32m"
+	local NO_COLOR="\033[0m"
+
+	echo -e "${GREEN_COLOR}$1${NO_COLOR}"
+}
+
+generate_docs() {
+    inform_user "Generating metrics"
+    composer run docs
+}
+
+generate_docs
+
+inform_user "All done!"
