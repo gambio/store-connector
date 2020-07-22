56e1035bbb09a5c62040472813b1d39cfcaea7cf
k.sekin <k.sekin@gambio.de>
2020-07-22 10:59:33 +0200

    Ensured the metrics script is consistent with other projects

--- .gitlab-ci/metrics.sh
+++ .gitlab-ci/metrics.sh
@@ -1,1 +1,22 @@
-composer metrics
+#!/bin/bash
+
+inform_user() {
+	if [[  -z "$1" ]]; then
+		print_usage "Empty argument provided"
+		exit 1
+	fi
+
+	local GREEN_COLOR="\033[0;32m"
+	local NO_COLOR="\033[0m"
+
+	echo -e "${GREEN_COLOR}$1${NO_COLOR}"
+}
+
+generate_metrics() {
+    inform_user "Geneating metrics"
+    composer metrics
+}
+
+generate_metrics
+
+inform_user "All done!"
