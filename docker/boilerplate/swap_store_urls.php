c75daa0ec75e5de18b9a7a4874fb00b3bbcf1ee6
c.schmidt <c.schmidt@gambio.de>
2020-08-07 13:43:40 +0200

    Added the store api url to the swap url script

--- docker/boilerplate/swap_store_urls.php
+++ docker/boilerplate/swap_store_urls.php
@@ -11,4 +11,8 @@ $pdo->query('UPDATE gm_configuration SET gm_value="http://' . $_ENV['DOCKER_HOST
 
 $pdo->query('UPDATE gx_configuration SET gm_value="http://' . $_ENV['DOCKER_HOST_IP'] . ':6900" WHERE gm_key="GAMBIO_STORE_URL"');
 
+$pdo->query('UPDATE gm_configuration SET gm_value="http://' . $_ENV['DOCKER_HOST_IP'] . ':6800" WHERE gm_key="APP_STORE_URL"');
+
+$pdo->query('UPDATE gx_configuration SET gm_value="http://' . $_ENV['DOCKER_HOST_IP'] . ':6800" WHERE gm_key="APP_STORE_URL"');
+
 echo "Success";
